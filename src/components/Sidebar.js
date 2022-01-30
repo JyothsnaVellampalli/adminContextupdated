722c4398b4480b7d5d34767cdbf1e8d7c195b207
Jyothsna <94163611+JyothsnaVellampalli@users.noreply.github.com>
2022-01-30 15:57:54 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,7 +6,7 @@ function Sidebar() {
   return (
     <div>
       <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.js">
           <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-laugh-wink"></i></div>
           <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
         </a>
